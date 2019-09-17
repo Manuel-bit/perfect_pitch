1d67e4941d50673a1a83fedaaa1070b78f745aba
manuel.bit <emmanuel_njuguna@outlook.com>
2019-09-17 13:14:27 +0300

    add postgress and shell to the pitch database

--- app/models.py
+++ app/models.py
@@ -1,13 +1,27 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 class User(db.Model):
   __tablename__='users'
   id = db.Column(db.Integer,primary_key=True)
   username= db.Column(db.String(255))
   email= db.Column(db.String(255))
-  password= db.Column(db.String(255))
+  pas_secure= db.Column(db.String(255))
   profile_pic= db.Column(db.String(100))
   pitches= db.relationship('Pitch',backref = 'user',lazy='dynamic')
+
+  @property
+  def password(self):
+    raise AttributeError('You cannot read the password attribute')
+
+  @password.setter
+  def password(self,password):
+    self.pass_secure = generate_password_hash(password)
+
+  def verify_password(self,password):
+    return check_password_hash(self.pass_secure,password)
+
 
 
   def __repr__(self):
