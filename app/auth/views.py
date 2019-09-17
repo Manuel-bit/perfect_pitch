23cfb08ea99f27177f2b936ec2e10c784d68ba83
manuel.bit <emmanuel_njuguna@outlook.com>
2019-09-17 13:52:01 +0300

    add the auth blueprint

---
+++ app/auth/views.py
@@ -0,0 +1,16 @@
+from flask import render_template,url_for,flash,redirect
+from .forms import SignUpForm,LoginForm
+from . import auth
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+
+  form = LoginForm()
+  if form.validate_on_submit():
+    if form.email.data == 'admin@blog.com and form.password.data' == 'password':
+      flash('you have been loged in successfully','success')
+      return redirect(url_for('home'))
+    else:
+      flash('login unsuccessfull. aithe email or password dont match','danger')  
+  return render_template('login.html', title='LOG IN', form=form)
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,10 @@
-from flask import render_template,url_for,flash,redirect
-from .forms import SignUpForm,LoginForm
+from flask import render_template
 from . import main
-
 
 @main.route('/')
 def home():
 
   return render_template('index.html')
-
 
 @main.route('/signup', methods=['GET','POST'])
 def signup():
@@ -18,17 +15,3 @@ def signup():
     return redirect(url_for('home'))
 
   return render_template('signup.html', title='SIGN UP', form=form)
-
-
-@main.route('/login',methods=['GET','POST'])
-def login():
-
-  form = LoginForm()
-  if form.validate_on_submit():
-    if form.email.data == 'admin@blog.com and form.password.data' == 'password':
-      flash('you have been loged in successfully','success')
-      return redirect(url_for('home'))
-    else:
-      flash('login unsuccessfull. aithe email or password dont match','danger')  
-  return render_template('login.html', title='LOG IN', form=form)
-
