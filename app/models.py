96ad8b4b882b7c5e0c5295f5f5558ac6a3cfe3a4
manuel.bit <emmanuel_njuguna@outlook.com>
2019-09-17 12:28:49 +0300

    add the pitch table

--- app/models.py
+++ app/models.py
@@ -1,6 +1,29 @@
 from . import db
 
-class User(db.model):
+class User(db.Model):
   __tablename__='users'
+  id = db.Column(db.Integer,primary_key=True)
+  username= db.Column(db.String(255))
+  email= db.Column(db.String(255))
+  password= db.Column(db.String(255))
+  profile_pic= db.Column(db.String(100))
+  pitches= db.relationship('Pitch',backref = 'user',lazy='dynamic')
+
+
+  def __repr__(self):
+    return f'User {self.username}'
+
+class Pitch(db.Model):
+  __tablename__='pitches'
+  id= db.Column(db.Integer, primary_key=True)
+  pitch= db.Column(db.String(255))
+  comment= db.Column(db.String(255))
+  category=db.Column(db.String(255))
+  downvote=db.Column(db.Integer)
+  upvote= db.Column(db.Integer)
+  user_id= db.Column(db.Integer,db.ForeignKey('users.id'))
   
+
+
+
 
